72e5217abccf829dbb4aaa79f45d0072940163cd
mayankmishra0801 <mayank0801mishra@gmail.com>
2024-10-25 20:37:23 +0530

    update and delete category

--- routes/category.js
+++ routes/category.js
@@ -2,19 +2,29 @@ const express = require("express");
 const router = express.Router();
 const Category = require("../db/category");
 
+const {addCategory,updateCategory, deleteCategory} = require("../handlers/category-handler");   
 
 
 router.post("",async(req,res)=>{
     console.log("here");
     let model  = req.body;
-    let category = new Category({
-        
-        name: model.name
-
-    })
-    category.save();
-    res.send(category.toObject());
+     let result = await addCategory(model)
+     
+    res.send(result);
 
 });
+
+router.put("/:id",async(req,res)=>{
+    let model = req.body;
+    let id = req.params["id"];
+    await updateCategory(id,model);
+    res.send({message:"ok"});
+})
+
+router.delete("/:id",async(req,res)=>{
+    let id =  req.params["id"];
+    await deleteCategory(id);
+    res.send({message:"deleted"});
+})
 
 module.exports = router;
